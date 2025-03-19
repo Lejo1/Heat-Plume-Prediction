4e89421308f9194a33a79090c314c2298399d34a
ja-tk <st166162@stud.uni-stuttgart.de>
2025-03-19 11:08:57 +0100

    UNet3d inherits from UNet

--- networks/unet3d.py
+++ networks/unet3d.py
@@ -1,10 +1,9 @@
 import torch.nn as nn
-from torch import save, tensor, cat, load, equal
-import pathlib
+from .unet import UNet
 
-class UNet3d(nn.Module):
+class UNet3d(UNet):
     def __init__(self, in_channels=4, out_channels=1, init_features=16, depth= 3, kernel_size=5):
-        super().__init__()
+        super().__init__(in_channels, out_channels, init_features, depth, kernel_size)
         features = init_features
         padding_mode =  "zeros"            
         self.encoders = nn.ModuleList()
@@ -25,21 +24,7 @@ class UNet3d(nn.Module):
 
         self.conv = nn.Conv3d(in_channels=features, out_channels=out_channels, kernel_size=1)
 
-    def forward(self, x: tensor) -> tensor:
-        encodings = []
-        for encoder, pool in zip(self.encoders, self.pools):
-            x = encoder(x)
-            encodings.append(x)
-            x = pool(x)
-        x = self.encoders[-1](x)
-
-        for upconv, decoder, encoding in zip(self.upconvs, self.decoders, reversed(encodings)):
-            x = upconv(x)
-            x = cat((x, encoding), dim=1)
-            x = decoder(x)
-
-        return self.conv(x)
-
+    
     @staticmethod
     def _block(in_channels, features, kernel_size=5, padding_mode="zeros"):
         return nn.Sequential(
@@ -48,7 +33,7 @@ class UNet3d(nn.Module):
                 out_channels=features,
                 kernel_size=kernel_size,
                 padding="same",
-                #padding_mode='reflect',
+                #padding_mode=padding_mode,
                 bias=True,
             ),
             nn.ReLU(inplace=True),
@@ -57,7 +42,7 @@ class UNet3d(nn.Module):
                 out_channels=features,
                 kernel_size=kernel_size,
                 padding="same",
-                #padding_mode='reflect',
+                #padding_mode=padding_mode,
                 bias=True,
             ),
             nn.BatchNorm3d(num_features=features),
@@ -67,52 +52,17 @@ class UNet3d(nn.Module):
                 out_channels=features,
                 kernel_size=kernel_size,
                 padding="same",
-                #padding_mode='reflect',
+                #padding_mode=padding_mode,
                 bias=True,
             ),        
             nn.ReLU(inplace=True),
         )
-    
-    def load(self, model_path:pathlib.Path, device:str = "cpu", model_name: str = "model.pt"):
-        self.load_state_dict(load(model_path/model_name))#,map_location="cpu"
-        self.to(device)
 
-    def save(self, path:pathlib.Path, model_name: str = "model.pt"):
-        save(self.state_dict(), path/model_name)
-
-        model_structure = []
-        for name, param in self.named_parameters():
-            model_structure.append([name, param.shape])
-        with open(path/"model_structure.txt", "w") as f:
-            f.write(str(model_structure))
-
-    def num_of_params(self):
-        return sum(p.numel() for p in self.parameters() if p.requires_grad)
-
-    def compare(self, model_2):
-        # source: https://discuss.pytorch.org/t/check-if-models-have-same-weights/4351/3
-        try:
-            # can handle both: model2 being only a state_dict or a full model
-            model_2 = model_2.state_dict()
-        except:
-            pass    
-        models_differ = 0
-        for key_item_1, key_item_2 in zip(self.state_dict().items(), model_2.items()):
-            if equal(key_item_1[1], key_item_2[1]):
-                pass
-            else:
-                models_differ += 1
-                if (key_item_1[0] == key_item_2[0]):
-                    print('Mismatch found at', key_item_1[0])
-                else:
-                    raise Exception
-        if models_differ == 0:  print('Models match perfectly! :)')
-
-def weights_init_3d(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        m.weight.data.normal_(0.0, 0.02)
-    elif classname.find("BatchNorm") != -1:
-        m.weight.data.normal_(1.0, 0.02)
-        m.bias.data.zero_()
+    def weights_init(self,m):
+        classname = m.__class__.__name__
+        if classname.find("Conv") != -1:
+            m.weight.data.normal_(0.0, 0.02)
+        elif classname.find("BatchNorm") != -1:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.zero_()
 
