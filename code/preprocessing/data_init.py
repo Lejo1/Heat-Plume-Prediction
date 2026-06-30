b8ccb3cd68d868afea8d6b8984458961c7e06243
Julia <jupelzer@outlook.de>
2026-06-30 14:17:27 +0200

    add option: take order_data: null and do random split and: improved data check: for matching names

--- code/preprocessing/data_init.py
+++ code/preprocessing/data_init.py
@@ -1,26 +1,48 @@
-from torch.utils.data import DataLoader
+import os
+import numpy as np
 
-from preprocessing.datasets.dataset import DataPoint
+from torch.utils.data import DataLoader, random_split
+from torch import Generator
+
+from preprocessing.datasets.dataset import DataPoint, DatasetBasis
 from preprocessing.datasets.dataset_cuts_jit import SimulationDatasetCuts
 
-def init_data(args:dict, tmp_bool_cutouts:bool=False, batchsize:int=64, ORDER_DATA:list=[0,2,1]):
+def init_data(args:dict, tmp_bool_cutouts:bool=False, batchsize:int=64, order_data:list=[0,2,1]):
     dataloaders = {}
     
     if not tmp_bool_cutouts or args["case"] == "test": # NO CUTOUTS
-        dataset_train = DataPoint(args["data_prep"], i=ORDER_DATA[0])
-        
+        if order_data:
+            dataset_train = DataPoint(args["data_prep"], i=order_data[0])
+        else: # large dataset with random split, no order_data provided
+            dataset = DatasetBasis(args["data_prep"])
+            datasets = random_split(dataset, [0.7, 0.2, 0.1], generator=Generator().manual_seed(1))
+            print(f"Random split of dataset: {len(datasets[0])} training, {len(datasets[1])} validation, {len(datasets[2])} test samples.")
+            
     else: # DO CUTOUTS
-        dataset_train = SimulationDatasetCuts(args["data_prep"], skip_per_dir=args["skip_per_dir"], box_size=args["len_box"], ids=ORDER_DATA[0])
-    dataset_val = DataPoint(args["data_prep"], i=ORDER_DATA[1])
-    datasets = [dataset_train, dataset_val]
+        if not order_data:
+            # load list of names in dir args[data_prep], then split into train/val/test
+            # runs = [int(f.split(".pt")[0].split("Sim_")[1]) for f in os.listdir(args["data_prep"]/"Inputs") if f.endswith(".pt")]
+            runs = np.arange(len([f for f in os.listdir(args["data_prep"]/"Inputs") if f.endswith(".pt")]))
+            np.random.shuffle(runs)
+            order_data = runs[:int(len(runs)*0.7)], runs[int(len(runs)*0.7):int(len(runs)*0.9)], runs[int(len(runs)*0.9):]
+        dataset_train = SimulationDatasetCuts(args["data_prep"], skip_per_dir=args["skip_per_dir"], box_size=args["len_box"], ids=order_data[0])
+        
+    if order_data:
+        dataset_val = DataPoint(args["data_prep"], i=order_data[1])
+        datasets = [dataset_train, dataset_val]
+    else: pass # already defined
 
     dataloaders["train"] = DataLoader(datasets[0], batch_size=batchsize, shuffle=True, num_workers=0)
     dataloaders["val"] = DataLoader(datasets[1], batch_size=batchsize, shuffle=True, num_workers=0)
-    try:
-        dataset_test = DataPoint(args["data_prep"], i=ORDER_DATA[2])
-        datasets.append(dataset_test)
-        dataloaders["test"] = DataLoader(datasets[2], batch_size=batchsize, shuffle=False, num_workers=0)
-    except:
-        pass
 
-    return datasets[0].input_channels, datasets[0].output_channels, dataloaders
+    # test dataset + dataloader
+    if order_data:
+        dataset_test = DataPoint(args["data_prep"], i=order_data[2])
+        datasets.append(dataset_test)
+    else: pass # already defined
+    dataloaders["test"] = DataLoader(datasets[2], batch_size=batchsize, shuffle=False, num_workers=0)
+
+    if order_data:
+        return datasets[0].input_channels, datasets[0].output_channels, dataloaders
+    else: # due to formality
+        return datasets[0].dataset.input_channels, datasets[0].dataset.output_channels, dataloaders
--- code/preprocessing/datasets/dataset.py
+++ code/preprocessing/datasets/dataset.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import torch
+import numpy as np
 import yaml
 from torch.utils.data import Dataset
 
@@ -8,28 +9,25 @@ from preprocessing.transforms import NormalizeTransform
 from utils.utils_args import get_run_ids_from_prep
 
 class DatasetBasis(Dataset):
-    def __init__(self, path:str, box_size:int=None, idx:int=None):
+    def __init__(self, path:str, box_size:int=None):
         Dataset.__init__(self)
         self.path = pathlib.Path(path)
         self.info = self.__load_info()
         self.norm = NormalizeTransform(self.info)
-        self.input_names = []
-        self.label_names = []
-        for filename in os.listdir(self.path / "Inputs"):
-            self.input_names.append(filename)
-        for filename in os.listdir(self.path / "Labels"):
-            self.label_names.append(filename)
+        self.input_names = [filename for filename in os.listdir(self.path / "Inputs") if filename.endswith(".pt")]
+        self.label_names = [filename for filename in os.listdir(self.path / "Labels") if filename.endswith(".pt")]
         self.input_names.sort()
         self.label_names.sort()
-        self.spatial_size = torch.load(self.path / "Labels" / self.input_names[0]).shape[1:]
+        self.__validate_matching_names()
+        self.cache = {}
+
+        tmp_dp = torch.load(self.path / "Labels" / self.label_names[0])
+        self.n_output_channels = tmp_dp.shape[0]
+        self.spatial_size = tmp_dp.shape[1:] # required for extend1,2 # TODO check if still works for extend (changed from Inputs to Labels for allin1)
         if box_size is not None:
             self.box_size = box_size
         else:
             self.box_size = self.spatial_size[0]
-
-        if len(self.input_names) != len(self.label_names):
-            raise ValueError(
-                "Number of Inputs and labels does not match!")
 
     @property
     def input_channels(self):
@@ -44,6 +42,21 @@ class DatasetBasis(Dataset):
             info = yaml.safe_load(f)
         return info
 
+    def __validate_matching_names(self):
+        input_names = set(self.input_names)
+        label_names = set(self.label_names)
+        if input_names != label_names:
+            missing_labels = sorted(input_names - label_names)
+            missing_inputs = sorted(label_names - input_names)
+            raise ValueError(
+                "Inputs and labels do not match by filename. "
+                f"Missing labels for: {missing_labels}. "
+                f"Missing inputs for: {missing_inputs}."
+            )
+
+    def _run_ids(self):
+        return get_run_ids_from_prep(self.path / "Inputs")
+
     def __len__(self):
         return len(self.input_names)
     
@@ -56,13 +69,15 @@ class DataPoint(DatasetBasis):
     def __init__(self, path:str, i:int=0):
         DatasetBasis.__init__(self, path)
         if isinstance(i, int):
-            run_id = get_run_ids_from_prep(self.path / "Inputs")[i]
+            run_id = self._run_ids()[i]
             
             self.input_names = [f"Sim_{run_id}.pt"]
             self.label_names = [f"Sim_{run_id}.pt"]
-        elif isinstance(i, list):
-            self.input_names = [f"Sim_{get_run_ids_from_prep(self.path / "Inputs")[ii]}.pt" for ii in i]
-            self.label_names = [f"Sim_{get_run_ids_from_prep(self.path / "Labels")[ii]}.pt" for ii in i]
+        elif isinstance(i, list) or isinstance(i, torch.Tensor) or isinstance(i, np.ndarray):
+            indices = i.tolist() if hasattr(i, "tolist") else i
+            run_ids = self._run_ids()
+            self.input_names = [f"Sim_{run_ids[ii]}.pt" for ii in indices]
+            self.label_names = [f"Sim_{run_ids[ii]}.pt" for ii in indices]
         else:
             raise ValueError("i must be an int or a list of ints")
         self.input_names.sort()
